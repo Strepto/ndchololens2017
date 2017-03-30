689e24ff204bb515a31ab954fbd6068eb7fa394e
HALS Nils Henrik <nils.hals@soprasteria.com>
2017-03-30 15:31:45 +0200

    Initial Commit

---
+++ Assets/Scripts/HoloAndSeekScripts.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class FoundTarget
+{
+    GameObject target;
+    int timestamp;
+    int number;
+    int points;
+    int pointsTotal;
+}
+
+public class HoloAndSeekScripts : MonoBehaviour
+{
+    public GameObject Score3DTextPrefab;
+    public GameObject Remaining3DTextPrefab;
+    
+    public List<GameObject> AllTargets;
+    public List<GameObject> UsedTargets = new List<GameObject>();
+    int score;
+    float startedTime;
+    List<FoundTarget> targetSequence;
+    GameObject currrentTarget;
+    System.Random rng = new System.Random();
+
+    // Use this for initialization
+    void Start()
+    {
+        Remaining3DTextPrefab.GetComponent<TextMesh>().text = UsedTargets.Count + " / " + AllTargets.Count;
+        foreach (var t in AllTargets)
+        {
+            var ts = t.GetComponent<TargetScript>();
+            if (ts == null)
+            {
+                throw new System.Exception("Target does not have a target Script");
+            }
+            
+            //TargetScript ts = t.AddComponent<TargetScript>();
+            ts.targetSeen.AddListener(FoundTarget);
+            t.SetActive(false);
+        };
+        currrentTarget = AllTargets[rng.Next(0, AllTargets.Count)];
+        currrentTarget.SetActive(true);
+
+        UsedTargets.Add(currrentTarget);
+        startedTime = Time.time;
+    }
+
+    public void FoundTarget()
+    {
+        Remaining3DTextPrefab.GetComponent<TextMesh>().text = UsedTargets.Count + " / " + AllTargets.Count;
+        currrentTarget.GetComponent<TargetScript>().targetSeen.RemoveListener(FoundTarget);
+        currrentTarget.GetComponent<Renderer>().enabled = false;
+        UsedTargets.Add(currrentTarget);
+        var allUnusedTargets = AllTargets.Except(UsedTargets);
+        currrentTarget = allUnusedTargets.ElementAt(rng.Next(0, allUnusedTargets.Count()));
+        currrentTarget.SetActive(true);
+        score = score + 10 + rng.Next(0,5);
+        Score3DTextPrefab.GetComponent<TextMesh>().text = "Score:" + score;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/TargetScript.cs
@@ -0,0 +1,46 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(ParticleSystem))]
+[RequireComponent(typeof(AudioSource))]
+public class TargetScript : MonoBehaviour, IFocusable {
+
+    ParticleSystem particleSystem;
+    AudioSource audioSource;
+    private bool targetHasBeenFound = false;
+    public UnityEvent targetSeen = new UnityEvent();
+
+
+    void IFocusable.OnFocusEnter()
+    {
+        if (targetHasBeenFound == false)
+        {
+            particleSystem.Play();
+            audioSource.Play();
+
+            targetSeen.Invoke();
+            targetHasBeenFound = true;
+        }
+    }
+
+    void IFocusable.OnFocusExit()
+    {
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        particleSystem = GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
